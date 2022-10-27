6de0cf8c9483d910d09d75abd5a2767a0cfd33a6
Jeevan Badial <jeevanbadial@gmail.com>
2022-10-27 13:00:05 +0100

    Use regex to replace parameter names in solution code with user's parameter names.
    This also applies to the displayed test cases.

--- src/klee_web/worker/storage/challenge_solutions.py
+++ src/klee_web/worker/storage/challenge_solutions.py
@@ -1,32 +1,34 @@
+import re
+
 challenges = {"max_challenge.c": r"""
 #include <klee/klee.h>
 #include <assert.h>
 
-int sample_max(int a, int b) {
-  if (a > b) {
-    return a;
+int sample_max(int param1, int param2) {
+  if (param1 > param2) {
+    return param1;
   } else {
-    return b;
+    return param2;
   }
 }
 
 int main() {
-  int a;
-  int b;
-  klee_make_symbolic(&a, sizeof(a), "a");
-  klee_make_symbolic(&b, sizeof(b), "b");
-  assert(sample_max(a, b) == max(a, b));
+  int param1;
+  int param2;
+  klee_make_symbolic(&param1, sizeof(param1), "param1");
+  klee_make_symbolic(&param2, sizeof(param2), "param2");
+  assert(sample_max(param1, param2) == max(param1, param2));
   return 0;
 }
 """,
-              "fibonacci_challenge": r"""
+              "fibonacci_challenge.c": r"""
 #include<klee/klee.h>
 #include<assert.h>
 
-int sample_fib(int n) {
+int sample_fib(int param1) {
   int a = 0;
   int b = 1;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < param1; i++) {
     b += a;
     a = b - a;
   }
@@ -34,14 +36,23 @@ int sample_fib(int n) {
 }
 
 int main() {
-  int a;
-  klee_make_symbolic(&a, sizeof(a), "a");
-  if (a >= 0 && a < 100) {
-    assert(sample_fib(a) == fibonacci(a));
+  int param1;
+  klee_make_symbolic(&param1, sizeof(param1), "param1");
+  if (param1 >= 0 && param1 < 100) {
+    assert(sample_fib(param1) == fibonacci(param1));
   }
   return 0;
 }"""}
 
 
 def add_solution_code(code, challenge_name):
-    return code + challenges.get(challenge_name, "")
+    sample_code = challenges.get(challenge_name, "")
+    challenge_func = challenge_name.replace("_challenge.c", "")
+    param_search = re.search(challenge_func + r"\s*\(([^)]*)", code)
+    if param_search:
+        params = param_search.group(1)
+        param_list = re.findall(r"[^)a-z]*\w*\s*(\w*)", params)
+        for i in range(len(param_list)):
+            sample_code = \
+                sample_code.replace("param" + str(i + 1), param_list[i])
+    return code + sample_code
