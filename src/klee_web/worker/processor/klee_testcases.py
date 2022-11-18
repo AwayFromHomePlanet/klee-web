8d2fd2730fe514c54de7cc288210566eabdecd3e
Istvan2471 <vander2471@gmail.com>
2022-11-18 13:59:52 +0000

    Began filtering testcase output representation

--- src/klee_web/worker/processor/klee_testcases.py
+++ src/klee_web/worker/processor/klee_testcases.py
@@ -2,7 +2,7 @@ import os
 import re
 
 from worker.processor.base import BaseProcessor
-
+from worker.storage.displayed_types import displayed_type_mappings
 
 class KleeTestCaseProcessor(BaseProcessor):
     name = 'test_cases'
@@ -21,8 +21,7 @@ class KleeTestCaseProcessor(BaseProcessor):
         ktest_output = self.runner.run_with_docker(ktest_tool_command)
         return self.parse_ktest(ktest_output)
 
-    @staticmethod
-    def parse_ktest(data):
+    def parse_ktest(self, data):
         def get_kv_pairs(strList):
             return [[x.strip(" \n") for x in pair.split(': ', 2)[1:]]
                     for pair in strList]
@@ -31,14 +30,33 @@ class KleeTestCaseProcessor(BaseProcessor):
         ktestDesc = data[0:3]
         memObjs = []
         i = 3
+
+        code_file = self.runner.DOCKER_CODE_FILE
+        with open(code_file) as f:
+            user_code = f.read()
+
+        user_param_types = self.parse_user_param_types(user_code)
+
+        param_index = 0
         while i < len(data):
             obj = dict(get_kv_pairs(data[i:i+2]))
             offset = 7 if int(obj["size"]) in [1, 2, 4, 8] else 5
-            obj["representations"] = get_kv_pairs(data[i+2:i+offset])
+            obj["representations"] = \
+                [rep for rep in get_kv_pairs(data[i+2:i+offset]) \
+                    if rep[0] in displayed_type_mappings[user_param_types[param_index]]]
             memObjs.append(obj)
             i += offset
+            param_index += 1
         return {"desc": ktestDesc, "mem_objs": memObjs}
 
+    def parse_user_param_types(self, code):
+        params = re.search(r"\s*\(([^)]*)\s*\)\s*\{", code)
+        if params:
+            params = params.group(1)
+            param_list = re.findall(r"\s*(\w+)\s+(\w+)\s*(?:,|$)", params)
+            param_types = map(re.split(r"\s+")[0], param_list)
+            return param_types
+    
     def process(self):
         ktest_files = filter(lambda f: f.endswith('.ktest'),
                              os.listdir(self.klee_result_dir))
---
+++ src/klee_web/worker/storage/displayed_types.py
@@ -0,0 +1,18 @@
+import re
+
+displayed_type_mappings = {
+    "int": [
+        "name",
+        "size",
+        "hex",
+        "int",
+        "uint" 
+    ],
+    "char": [
+        "name",
+        "size",
+        "hex",
+        "int",
+        "text"
+    ]
+}
